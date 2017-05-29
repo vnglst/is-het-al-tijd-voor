878215d6967d480987188792f7cb0d34a09536af
Koen van Gilst <koen.vangilst@gmail.com>
2017-05-29 13:39:59 +0200

    moves ah api calls into app

--- pages/asperges.js
+++ pages/asperges.js
@@ -1,8 +1,6 @@
-// import Layout from '../components/MyLayout.js'
-import fetch from 'isomorphic-unfetch'
+const ah = require('../utils/ah.js')
 
 const Post = (props) => {
-  console.log(props)
   return (
     <div>
       <h1>Ja,</h1><span>en bij de AH kosten ze momenteel {props.priceInfo.price}</span>
@@ -11,11 +9,10 @@ const Post = (props) => {
 }
 
 Post.getInitialProps = async function (context) {
-//   const { id } = context.query
-  const res = await fetch(`https://albert-heijn.now.sh/ah/price?productId=wi99045&productUrl=https://www.ah.nl/producten/product/wi99045/ah-asperges-wit`)
-//   console.log(res)
-  const priceInfo = await res.json()
-  console.log(`Fetched price info: ${priceInfo}`)
+  const url = 'https://www.ah.nl/producten/product/wi99045/ah-asperges-wit'
+  const productId = 'wi99045'
+  const priceInfo = await ah.getPriceInfo(url, productId)
+  console.log(`Fetched price info: ${priceInfo.price}`)
   return { priceInfo }
 }
 
---
+++ utils/ah.js
@@ -0,0 +1,51 @@
+import fetch from 'isomorphic-unfetch'
+import urlTools from 'url'
+import { map, has, flatten } from 'lodash'
+
+/*
+  Deep search an object for a certain key
+   -> returns array of objects containing key
+*/
+function findKey (obj, key) {
+  if (has(obj, key)) return [obj]
+  return flatten(
+    map(obj, function (v) {
+      return typeof v === 'object' ? findKey(v, key) : []
+    }),
+    true
+  )
+}
+
+const getJSONUrl = url => {
+  const { pathname } = urlTools.parse(url)
+  return `https://www.ah.nl/service/rest/delegate?url=${encodeURIComponent(pathname)}`
+}
+
+const getJSON = async url => {
+  const response = await fetch(getJSONUrl(url))
+  if (response.status >= 400) {
+    throw new Error('Bad response from server')
+  }
+  return response.json()
+}
+
+const getProductInfo = async (url, productId) => {
+  const json = await getJSON(url)
+  const allProducts = findKey(json, 'id')
+  const productInfo = allProducts.find(product => product.id === productId)
+  return productInfo
+}
+
+const getPriceInfo = async (url, productId) => {
+  const productInfo = await getProductInfo(url, productId)
+  return {
+    price: productInfo.priceLabel.now,
+    unit: productInfo.unitSize,
+    availability: productInfo.availability
+  }
+}
+
+module.exports = {
+  getPriceInfo,
+  getProductInfo
+}
